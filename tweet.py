04dfc73b477d01a705d94b7e92348132acfba37a
romanorac <roman.orac@outlook.com>
2015-01-13 17:43:18 +0000

    first gen bot

--- tweet.py
+++ tweet.py
@@ -27,45 +27,65 @@ ccpattern = (
     "5ZnXSFeyHOFLjZ8dh7AzscYt88xOhZhBRMWxbOu86pE0z")
 )
 
-# @ccpattern is followed by Selena Gomez!
-# We better post some good stuff so she notices us.
-# Tip: we could create a bot that follows everyone,
-# hope they follow us back, and then invent tweets that address them.
+def post_tweet(tweet):
+	from pattern.web import URL, Twitter
 
-from pattern.web import URL, Twitter, MODULE
-print MODULE
+	url = URL("https://api.twitter.com/1.1/statuses/update.json", method="post", query={"status": tweet})
 
-# Tweet to post:
-tweet = "I am alive... alive! #codecampcc"
+	twitter = Twitter(license=ccpattern)
+	url = twitter._authenticate(url)
 
-# The API for posting is described here:
-# # https://dev.twitter.com/rest/reference/post/statuses/update
-url = URL("https://api.twitter.com/1.1/statuses/update.json", method="post", query={"status": tweet})
-
-# We'll use the Twitter._authenticate() method to authenticate ourselves 
-# as @ccpattern (so the new tweet will appear on @ccpattern's page):
-twitter = Twitter(license=ccpattern)
-
-url = twitter._authenticate(url)
+	try:
+	    # Send the post request.
+	    url.open()
+	except Exception as e:
+	    print e
+	    print e.src
+	    print e.src.read()
 
 
-try:
-    # Send the post request.
-    url.open()
-except Exception as e:
-    print e
-    print e.src
-    print e.src.read()
-    
-# To create your own Twitter bot:
 
-# 1) You need a new e-mail address for the bot (e.g., gmail.com).
-# 2) You need a new Twitter account.
-# 3) Verify the Twitter account from the e-mail they send you.
-# 4) Verify the Twitter account with a mobile phone number (this is mandatory): 
-#   https://support.twitter.com/articles/110250-adding-your-phone-number-to-your-account
-# 5) While logged in in the new account, create a Twitter App:
-#   https://apps.twitter.com/app/new
-# 6) Modify the app's permissions to "read & write".
-# 7) Regenerate the keys and access tokens.
-# 8) Paste the keys and access tokens into this script instead of @ccpattern's keys.
+
+if __name__ == '__main__':
+	# @ccpattern is followed by Selena Gomez!
+	# We better post some good stuff so she notices us.
+	# Tip: we could create a bot that follows everyone,
+	# hope they follow us back, and then invent tweets that address them.
+	from pattern.web import URL, Twitter
+
+	# Tweet to post:
+	tweet = "I am alive... alive! #codecampCC"
+
+	# The API for posting is described here:
+	# # https://dev.twitter.com/rest/reference/post/statuses/update
+
+
+	url = URL("https://api.twitter.com/1.1/statuses/update.json", method="post", query={"status": tweet})
+
+	# We'll use the Twitter._authenticate() method to authenticate ourselves 
+	# as @ccpattern (so the new tweet will appear on @ccpattern's page):
+	twitter = Twitter(license=ccpattern)
+
+	url = twitter._authenticate(url)
+
+
+	try:
+	    # Send the post request.
+	    url.open()
+	except Exception as e:
+	    print e
+	    print e.src
+	    print e.src.read()
+	    
+	# To create your own Twitter bot:
+
+	# 1) You need a new e-mail address for the bot (e.g., gmail.com).
+	# 2) You need a new Twitter account.
+	# 3) Verify the Twitter account from the e-mail they send you.
+	# 4) Verify the Twitter account with a mobile phone number (this is mandatory): 
+	#   https://support.twitter.com/articles/110250-adding-your-phone-number-to-your-account
+	# 5) While logged in in the new account, create a Twitter App:
+	#   https://apps.twitter.com/app/new
+	# 6) Modify the app's permissions to "read & write".
+	# 7) Regenerate the keys and access tokens.
+	# 8) Paste the keys and access tokens into this script instead of @ccpattern's keys.
