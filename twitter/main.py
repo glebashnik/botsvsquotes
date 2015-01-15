cf64f4c6152dd85fd2ca703feabd62ace45309db
glebashnik <glebsv@gmail.com>
2015-01-15 07:57:45 +0000

    Added some comments.

--- twitter/main.py
+++ twitter/main.py
@@ -1,3 +1,8 @@
+# Black card - quote with the blank.
+# White card - proposed filling for the quote in the black card.
+# Czar - the character with the black card, also the one who selects a winner
+# Players - all the other characters except czar
+
 import random
 from time import sleep
 
@@ -18,9 +23,9 @@ character_sim = Sim()
 
 def select_players(czar, candidates):
     czar_profile = kb.get_people_from_kb([czar])[0]
-    candidate_profiles = kb.get_people_from_kb(candidates)
-    player_profiles = character_sim.choose_players_random(czar_profile, candidate_profiles, 5)
-    return [p["Character"] for p in player_profiles]
+    player_profiles = kb.get_people_from_kb(candidates)
+    selected_player_profiles = character_sim.choose_players_random(czar_profile, player_profiles, 5)
+    return [p["Character"] for p in selected_player_profiles]
     #return random.sample(candidates, 5)
 
 
@@ -37,17 +42,18 @@ def select_winner(czar, czar_card, selected_players_cards):
     return max(selected_players_cards.keys(), key=lambda p: match_cards(czar_card, selected_players_cards[p]))
     #return random.choice(selected_players_cards.keys())
 
+
 if __name__ == '__main__':
     while 1:
         try:
             print "playing"
             characters = load_character_names()
             czar = select_czar(characters)
-            candidates = [character for character in characters if czar != character]
-            players = select_players(czar, candidates)
+            players = [character for character in characters if czar != character]
+            selected_players = select_players(czar, players)
 
             czar_cards = load_character_cards(czar)
-            players_cards = {player: load_character_cards(player) for player in players}
+            players_cards = {player: load_character_cards(player) for player in selected_players}
 
             selected_czar_card = select_czar_card(czar_cards)
             selected_players_cards = {player: select_player_card(selected_czar_card, players_cards)
